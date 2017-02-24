6aec86e0828cf1edb597e4a8ae521eda69c035f4
Jonathan Tripathy <jt@puppet.com>
2017-02-24 16:43:24 +0000

    Add code to check CRL

--- manifests/eap.pp
+++ manifests/eap.pp
@@ -2,14 +2,16 @@
 # ===========================
 
 class freeradius::eap (
-  String  $package                  = "$::freeradius::params::package",
-  String  $service                  = "$::freeradius::params::service",
-  String  $conf_dir                 = "$::freeradius::params::conf_dir",
-  String  $private_key_source_path  = "${::settings::confdir}/ssl/private_keys/${::clientcert}.pem",
-  String  $cert_file_source_path    = "${::settings::confdir}/ssl/certs/${::clientcert}.pem",
-  String  $ca_file_source_path      = "${::settings::confdir}/ssl/certs/ca.pem",
-  String  $pki_dir                  = "${::freeradius::params::conf_dir}/pki",
-  Boolean $peap_mschapv2            = false,
+  String   $crl_file_source_path            = "${::settings::confdir}/ssl/ca/ca_crl.pem",
+  Boolean  $crl_local_web_fetch_enabled     = false,
+  String   $package                         = "$::freeradius::params::package",
+  String   $service                         = "$::freeradius::params::service",
+  String   $conf_dir                        = "$::freeradius::params::conf_dir",
+  String   $private_key_source_path         = "${::settings::confdir}/ssl/private_keys/${::clientcert}.pem",
+  String   $cert_file_source_path           = "${::settings::confdir}/ssl/certs/${::clientcert}.pem",
+  String   $ca_file_source_path             = "${::settings::confdir}/ssl/certs/ca.pem",
+  String   $pki_dir                         = "${::freeradius::params::conf_dir}/pki",
+  Boolean  $peap_mschapv2                   = false,
 ) {
 
   concat { "${conf_dir}/eap.conf":
@@ -75,6 +77,30 @@ class freeradius::eap (
     notify   => Service[$service],
   }
 
+  if $crl_local_web_fetch_enabled {
+    exec {'update_crl_file':
+      command  => "/usr/bin/curl $crl_file_source_path -o ${pki_dir}/crl.pem --cacert ${pki_dir}/ca.crt ; shasum ${pki_dir}/crl.pem | awk '{print \$1}' > ${pki_dir}/crl.pem.sha",
+      unless   => "/usr/bin/curl $crl_file_source_path --cacert ${pki_dir}/ca.crt | shasum | awk '{print \$1}' | diff ${pki_dir}/crl.pem.sha -",
+      require  => File["${pki_dir}/ca.crt"],
+      notify   => Exec['rehash_crl'],
+    }
+  } else {
+    file {"${pki_dir}/crl.pem":
+      ensure    => present,
+      contents  => file($crl_file_source_path),
+      owner     => 'freerad',
+      mode      => '0600',
+      require   => File[$pki_dir],
+      notify    => Exec['rehash_crl'],
+    }
+  }
+
+  exec {'rehash_crl':
+    command      => "/usr/bin/c_rehash $pki_dir",
+    refreshonly  => true,
+    notify       => Service[$service],
+  }
+
   concat::fragment {'eap_tls_conf':
     target  => "${conf_dir}/eap.conf",
     content => epp('freeradius/eap_tls.epp', {
@@ -82,6 +108,7 @@ class freeradius::eap (
       'cert_file_path'    => "${pki_dir}/server.crt",
       'ca_file_path'      => "${pki_dir}/ca.crt",
       'dh_file_path'      => "${pki_dir}/dh",
+      'pki_dir'           => "${pki_dir}",
     }),
     order   => '03'
   }
