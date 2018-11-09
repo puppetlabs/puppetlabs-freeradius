569050b5118290abc271c4b7e32bfd42cb9acec8
Jonathan Tripathy <jt@puppet.com>
2018-11-09 13:16:30 -0800

    Made params file more generic for Debian-based OSes

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,9 @@
 class freeradius::params {
   case $::osfamily {
     'Debian': {
-      case $::operatingsystem {
-        'Debian': {
-          if versioncmp($::operatingsystemrelease, '8.0') >= 0 {
-            $package   = 'freeradius'
-            $service   = 'freeradius'
-            $conf_dir  = '/etc/freeradius'
-          }
-        }
-      }
+      $package   = 'freeradius'
+      $service   = 'freeradius'
+      $conf_dir  = '/etc/freeradius'
     }
   }
 }
